ef78f23a0d4c10297a97622c43bd77b0184cc48f
Brandon Fredericks <brandon.fredericks@mapmyfitness.com>
2013-06-10 21:10:51 -0600

    Updated the version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 setupArgs = {
     'name': 'zuora',
-    'version': '1.0.0.1',
+    'version': '1.0.0.2',
     'author': 'MapMyFitness',
     'author_email': 'brandon.fredericks@mapmyfitness.com',
     'url': 'http://github.com/mapmyfitness/python-zuora',
