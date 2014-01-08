d60edf576b838807b2623600eff7d8ab5d43a3ab
Dylan Mankey <dylan.mankey@mapmyfitnessinc.com>
2014-01-08 17:40:02 -0600

    updating version from 0.9 to 0.14 due to some strange state in pip-freeze-prod which was pointing at version 0.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 setupArgs = {
     'name': 'mmf-zuora',
-    'version': '1.0.0.9',
+    'version': '1.0.0.14',
     'author': 'MapMyFitness',
     'author_email': 'brandon.fredericks@mapmyfitness.com',
     'url': 'http://github.com/mapmyfitness/python-zuora',
