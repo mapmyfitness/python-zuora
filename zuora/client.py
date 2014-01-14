1af3cd6a8c02c6ccd7c704c62631366d5b2494d4
Brandon Fredericks <brandon.fredericks@mapmyfitness.com>
2014-01-13 17:32:16 -0700

    Moved the timeout.

--- zuora/client.py
+++ zuora/client.py
@@ -46,7 +46,7 @@ class HttpTransportWithKeepAlive(HttpAuthenticated, object):
 
     def __init__(self):
         super(HttpTransportWithKeepAlive, self).__init__()
-        self.http = httplib2.Http()
+        self.http = httplib2.Http(timeout=1)
 
     def open(self, request):
         return HttpTransport.open(self, request)
